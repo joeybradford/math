6f18303da9bf85a375323fcb4e14c07db38e6133
joeybradford <bradfojh@dukes.jmu.edu>
2016-04-27 10:32:34 -0400

    Added files via upload

---
+++ math252/spec/math_spec.js
@@ -0,0 +1,26 @@
+//tell spec where to find the code
+var math = require('../math');
+
+describe("A simple arithmetic program", function() {
+	
+	it("can add two numbers",function(){
+		expect(math.add(3,5)).toBe(8);
+		expect(math.add(3.2,5.7)).toBe(8.9);
+		expect(math.add(3,5)).toBe(8);
+
+	});
+	it("can subtract two numbers",function(){
+		expect(math.subtract(3,5)).toBe(-2);
+		expect(math.subtract(3.5,5)).toBe(-1.5;
+	});
+	it("can multiply 2 numbers",function(){
+		expect(math.multiply(3,5)).toBe(15);
+		expect(math.multiply(3,0)).toBe(0);
+		expect(math.multiply(3.3,5)).toBe(16.5);	
+	});
+	it("can divide two numbers",function(){
+		expect(math.divide(15,5)).toBe(3);
+		expect(math.divide(15,0)).toBe("error");		
+	});
+	
+});
